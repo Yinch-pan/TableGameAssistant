e72a7a51ed80dd2f362dc682bbbbe4d1d0613059
yinch pan <yinchpan@gmail.com>
2024-02-16 22:17:04 +0800

    ~

---
+++ randrole/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def randomrole(request):
+    return render(request,'test.html')
+
+
+def addrole(request):
+    return render(request,'test.html')
